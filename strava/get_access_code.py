4a601fc04566a36497c084ba05311373aa08efc6
Matthew Falcioni <mfalcioni1@gmail.com>
2024-05-08 21:25:22 -0400

    moving along

--- strava/get_access_code.py
+++ strava/get_access_code.py
@@ -1,16 +1,15 @@
-# Access Strava
-One-time set-up. First create [a Strava App](https://medium.com/analytics-vidhya/accessing-user-data-via-the-strava-api-using-stravalib-d5bee7fdde17), and add the client ID and client secret to the `.env.sample` file and rename it to `.env`.
-
-```py
 from stravalib import Client
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
+# Retrieve client ID and client secret from environment variables
 client_id = os.getenv('STRAVA_CLIENT_ID')
 client_secret = os.getenv('STRAVA_CLIENT_SECRET')
 
+# Create a Strava client
 client = Client()
 
 url = client.authorization_url(client_id=client_id, 
@@ -18,5 +17,3 @@ url = client.authorization_url(client_id=client_id,
                                scope=['read_all','profile:read_all','activity:read_all'])
 
 print(url)
-```
-Running this generates a clickable URL that will redirect and show the access code. Add the access code to the `.env` file.
